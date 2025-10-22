62fd4fd1974b3c6d0340c0c26a72e9d8e58e7be1
Nguyen Van Quang <quangee210k66@gmail.com>
2025-10-22 14:08:47 +0700

    Initialize clean project structure

--- src/module/AxialMixer.py
+++
@@ -1,9 +0,0 @@
-class AxialMixer(nn.Module):
-    def __init__(self, dim, mixer_kernel, dilation = 1):
-        super().__init__()
-        h, w = mixer_kernel
-        self.mixer_h = nn.Conv2d(dim, dim, kernel_size=(h, 1), padding='same', groups = dim, dilation = dilation)
-        self.mixer_w = nn.Conv2d(dim, dim, kernel_size=(1, w), padding='same', groups = dim, dilation = dilation)
-    def forward(self, x):
-        x = x + self.mixer_h(x) + self.mixer_w(x)
-        return x
